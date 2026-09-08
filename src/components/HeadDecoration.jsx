0295875091bc2eca5a33d0e7edb11922863d21ca
Ruan Gomes <ruan.gmss@outlook.com>
2026-09-08 13:10:45 -0300

    refactor: Remmovi dados estáticos, incluindo dinâmicas

--- src/components/HeadDecoration.jsx
+++ src/components/HeadDecoration.jsx
@@ -1,12 +1,32 @@
+import React from 'react';
+
 const HeadDecoration = () => {
+  const today = new Date().toLocaleDateString('pt-BR', { weekday: 'long' }).replace('-feira', '');
+  const formattedToday = today.charAt(0).toUpperCase() + today.slice(1);
+
+  const weathers = [
+    { temperature: 28, emoji: '☀️' },
+    { temperature: 22, emoji: '🌤️' },
+    { temperature: 19, emoji: '🌧️' },
+    { temperature: 25, emoji: '⛅' },
+    { temperature: 31, emoji: '☀️' },
+    { temperature: 24, emoji: '🌤️' },
+    { temperature: 16, emoji: '🌧️' },
+    { temperature: 26, emoji: '⛅' },
+  ];
+
+  const [weather] = React.useState(() => {
+    return weathers[Math.floor(Math.random() * weathers.length)];
+  });
+
   return (
     <div className="container">
       <div className="flex gap-1 text-primary-300 [&>*:not(:first-child)]:bg-primary-900 [&>*:not(:first-child)]:px-5 [&>*:not(:first-child)]:py-2 [&>*:not(:first-child)]:rounded-lg relative">
         <span className="size-2 block rounded-full bg-primary-300 absolute top-4 -left-1"></span>
 
-        <span>Segunda</span>
-        <span>25º</span>
-        <span>🌤️</span>
+        <span>{formattedToday}</span>
+        <span>{weather.temperature}º</span>
+        <span>{weather.emoji}</span>
       </div>
     </div>
   );
