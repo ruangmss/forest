57783a8a190cbfd087e1d07cda4edb41691d201d
Ruan Gomes <ruan.gmss@outlook.com>
2026-09-09 13:48:22 -0300

    refactor: Ajusta o tamanho de imagens

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -7,7 +7,7 @@ import wildbeast from '../assets/partners/wildbeast.svg';
 const Partners = () => {
   return (
     <section className="container">
-      <div className="flex items-center p-6 justify-center gap-8 border-y-2 border-primary-900 [&>img]:min-w-0 max-md:flex-col max-md:border-0">
+      <div className="flex items-center p-6 justify-center gap-8 border-y-2 border-primary-900 [&>img]:min-w-0 [&>img]:max-w-36 max-md:flex-col max-md:border-0">
         <img src={caravan} alt="Logo Caravan" />
         <img src={dogs} alt="Logo Dogs" />
         <img src={lescone} alt="Logo Lescone" />
